c5f20b9124303a65482db98ffe32a1dd840d23e9
Sesso Kosga <kosgasesso@gmail.com>
2023-05-29 14:36:44 +0100

    Added the other dialogues

--- main.gd
+++ main.gd
@@ -5,17 +5,17 @@ var dialogue
 
 func _ready() -> void:
 	show_menu()
-	timeline  = Dialogic.preload_timeline("res://dialogues/dialogue.dtl")
-#	Dialogic.connect("timeline_ended",show_credits)	
-	start_game()
+	timeline  = Dialogic.preload_timeline("res://dialogues/dialogue.dtl")	
+	show_menu()
 
 func show_menu() -> void:
+	$Background.show()
 	$CreditsUI.hide()
 	$HomeUI.show()
 
 func show_credits()->void:
 	$HomeUI.hide()
-	
+	$Background.show()
 	$CreditsUI.show()
 	
 func _on_home_ui_credits() -> void:
@@ -27,7 +27,9 @@ func _on_home_ui_quit() -> void:
 func start_game()-> void:
 	$CreditsUI.hide()
 	$HomeUI.hide()
+	$Background.hide()
 	dialogue = Dialogic.start(timeline)
+	Dialogic.connect("timeline_ended",show_credits)
 	Dialogic.connect("signal_event",handle_dialogue_signal)
 	
 func _on_home_ui_start() -> void:
@@ -37,5 +39,5 @@ func handle_dialogue_signal(event_name:String) -> void:
 	match(event_name):
 		"show_victim_logo":
 			$Victim.show()
-		"show_victim_logo":
+		"hide_victim_logo":
 			$Victim.hide()
